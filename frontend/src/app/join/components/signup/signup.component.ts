eb07bd4f0ef47f0753b13581a4ebff755b3dc235
William Parker <wiparker2020@gmail.com>
2022-07-07 23:37:29 -0400

    Reasonable fix for sign up passwordConfirm

--- frontend/src/app/join/components/signup/signup.component.ts
+++ frontend/src/app/join/components/signup/signup.component.ts
@@ -1,60 +1,74 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class ConfirmPassErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control?.invalid && control?.parent?.dirty && control.touched);
+    const invalidParent = !!(control?.parent?.invalid && control?.parent?.dirty && control.touched);
+
+    const invalidusername = !!(control?.parent?.get('username')?.invalid);
+    const invalidEmail = !!(control?.parent?.get('email')?.invalid);
+    const invalidPass = !!(control?.parent?.get('password')?.invalid);
+
+    const invalidFinal = invalidParent && !(invalidusername || invalidEmail || invalidPass);
+
+    return invalidCtrl || invalidFinal;
+  }
+}
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signUpForm!: FormGroup;
 
   hide = true;
-  
-  username = new FormControl('', [Validators.required]);
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  passMatcher = new ConfirmPassErrorStateMatcher();
 
-  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  constructor(private formBuilder: FormBuilder) {}
 
-  passwordConfirm = new FormControl('', [Validators.required]);
+  checkPasswords: ValidatorFn = (control: AbstractControl):  ValidationErrors | null =>{
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('passwordConfirm')?.value;
 
-
-  getEmailErrorMessage() {
-    if (this.email.hasError('email')) {
-      return 'Not a valid email';
+    if((password === confirmPassword) && (password !== null && confirmPassword !== null)) {
+      return null;
+    } else {
+      return {passwordsNotMatching: true};
     }
-    if (this.email.hasError('required')) {
-      return 'You must enter a email';
-    }
-
-    return 'Unknown error';
   }
 
-  getUsernameErrorMessage() {
-    if (this.username.hasError('required')) {
-      return 'You must enter a username';
-    }
-
-    return 'Unknown error';
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group(
+      {
+        username: [null, [Validators.required]],
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required, Validators.minLength(8)]],
+        passwordConfirm: [null, [Validators.required]],
+      },
+      {
+        validators: this.checkPasswords,
+      }
+    );
   }
 
-  getPasswordErrorMessage() {
-    if (this.password.hasError('minlength')) {
-      return 'Minimum length is 8';
+  onSubmit() {
+    if (this.signUpForm.invalid) {
+      return;
     }
-    if (this.password.hasError('required')) {
-      return 'You must enter a password';
-    }
-
-    return 'Unknown error';
   }
-
-  getPasswordConfirmErrorMessage() {
-    if (this.passwordConfirm.hasError('required')) {
-      return 'Please re-enter your password';
-    }
-
-    return 'Unknown error';
-  }
-
 }
