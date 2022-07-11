942c95d0c936d2f76aff2f7e91078c2be67fdf05
William Parker <wiparker2020@gmail.com>
2022-07-11 18:36:33 -0400

    Start file structure.

---
+++ frontend/src/app/main/following/following.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FollowingRoutingModule } from './following-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FollowingRoutingModule
+  ]
+})
+export class FollowingModule { }
