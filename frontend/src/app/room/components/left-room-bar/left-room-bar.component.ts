fc20691203ab526299e7f81f975a18da9d810d87
William Parker <wiparker2020@gmail.com>
2022-07-16 13:48:05 -0400

    Adds visuals to when room is clicked.

--- frontend/src/app/room/components/left-room-bar/left-room-bar.component.ts
+++ frontend/src/app/room/components/left-room-bar/left-room-bar.component.ts
@@ -1,9 +1,8 @@
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { map } from 'rxjs';
-import { TreeRoomDto } from 'src/app/models';
 import { RoomService } from '../../service/room.service';
 import { RoomDto } from '../models/room.dto';
 
@@ -18,13 +17,16 @@ export class LeftRoomBarComponent implements OnInit {
 
   roomTree: RoomDto[] | undefined;
 
-  roomName: string | undefined;
+  rootRoomAddress: string | undefined;
+
+  currentRoom: string | undefined;
 
   param: any;
 
   constructor(
     private route: ActivatedRoute,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -32,7 +34,8 @@ export class LeftRoomBarComponent implements OnInit {
       .getRoomTree(this.route.snapshot.queryParams['hub'])
       .pipe(
         map((rooms: RoomDto) => {
-          this.roomName = rooms.name;
+          this.rootRoomAddress = rooms.roomAddress;
+          this.currentRoom = rooms.roomAddress;
           if (rooms.childRooms) {
             this.roomTree = rooms.childRooms;
             this.dataSource.data = this.roomTree;
@@ -46,6 +49,23 @@ export class LeftRoomBarComponent implements OnInit {
         this.ngOnInit();
       }
     });
+  }
+
+  changeRoom(address: string) {
+    this.currentRoom = address;
+    const queryParams: Params = { address: address };
+
+  this.router.navigate(
+    [], 
+    {
+      relativeTo: this.route,
+      queryParams: queryParams, 
+      queryParamsHandling: 'merge', // remove to replace all query params by provided
+    });
+  }
+
+  roomSelected(address: string) {
+    return address === this.currentRoom;
   }
 
   hasChild = (_: number, node: RoomDto) =>
